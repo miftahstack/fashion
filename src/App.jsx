3bb001f9ed884cb941f79a40152a248bcd6ac1b3
MIFTAH UL HAQ <miftah4165@gmail.com>
2026-08-19 18:43:27 +0600

    Added files annd favicon

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Routes, Route } from "react-router";
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
