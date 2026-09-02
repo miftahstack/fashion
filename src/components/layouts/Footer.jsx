e3f915c8d7bceac696ed531c8a87d97ffd5bed5e
MIFTAH UL HAQ <miftah4165@gmail.com>
2026-09-02 14:53:59 +0600

    Footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -14,11 +14,11 @@ const Footer = () => {
         <>
             <div className="bg-black">
                 <Container className={`py-25`}>
-                    <Flex>
+                    <Flex className={`text-white justify-between`}>
                         <div className="space-y-5">
                             <h4 className='text-white font-bold text-[30px]'>Fashion</h4>
                             <p className='text-[#8E8E8E]'>Complete your style with awesome clothes from us.</p>
-                            <Flex className={`gap-x-2`}>
+                            <Flex className={`gap-x-2 text-black`}>
                                 <div className="bg-[#EBD96B] p-2 rounded-[10px]">
                                     <FaFacebookF />
                                 </div>
@@ -33,9 +33,31 @@ const Footer = () => {
                                 </div>
                             </Flex>
                         </div>
-                        <div className=""></div>
-                        <div className=""></div>
-                        <div className=""></div>
+                        <div className="text-[#8E8E8E]">
+                            <ul>
+                                <li className='text-white'>Company</li>
+                                <li>About</li>
+                                <li>Contact us</li>
+                                <li>Support</li>
+                                <li>Careers</li>
+                            </ul>
+                        </div>
+                        <div className="text-[#8E8E8E]">
+                            <ul>
+                                <li className='text-white'>Quick Link</li>
+                                <li>Share Location</li>
+                                <li>Orders Tracking</li>
+                                <li>Size Guide</li>
+                                <li>FAQs</li>
+                            </ul>
+                        </div>
+                        <div className="text-[#8E8E8E]">
+                            <ul>
+                                <li className='text-white'>Legal</li>
+                                <li>Terms & conditions</li>
+                                <li>Privacy Policy</li>
+                            </ul>
+                        </div>
                     </Flex>
                 </Container>
             </div>
