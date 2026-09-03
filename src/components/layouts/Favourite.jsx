e7ddde1436e32900d08f51b9e398ebcd2aeb4f6f
MIFTAH UL HAQ <miftah4165@gmail.com>
2026-09-03 14:23:49 +0600

    Img added

--- src/components/layouts/Favourite.jsx
+++ src/components/layouts/Favourite.jsx
@@ -3,6 +3,11 @@ import Container from '../Container'
 import Flex from '../Flex'
 import { FaArrowRightLong } from 'react-icons/fa6'
 
+// Img
+import favgOne from '/src/assets/favgOne.png';
+import favgTwo from '/src/assets/favgOne.png';
+// Img
+
 const Favourite = () => {
     return (
         <>
@@ -10,7 +15,7 @@ const Favourite = () => {
                 <h3 className='text-[48px] uppercase font-bold mb-15'>Young’s Favourite</h3>
                 <Flex className={`gap-x-10`}>
                     <div className="">
-                        <img src="src/assets/favgTwo.png" alt="" />
+                        <img src={favgOne} alt="" />
                        <Flex className={`justify-between items-center`}>
                         <div className="">
                             <p className='text-[26px]'>Trend on instagram</p>
@@ -22,7 +27,7 @@ const Favourite = () => {
                     </Flex>
                     </div>
                     <div className="">
-                        <img src="src/assets/favgOne.png" alt="" />
+                        <img src={favgTwo} alt="" />
                         <p className='text-[24px] '>All Under $40</p>
                         <p className='text-[18px] text-[#7F7F7F]'>Explore Now!</p>
                     </div>
