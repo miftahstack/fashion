9cc3aa9eae914c5906cdfb2859cc3eba88215e98
MIFTAH UL HAQ <miftah4165@gmail.com>
2026-09-03 13:33:17 +0600

    src changes

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({ className, children }) => {
     return (
-        <div className={` max-w-[1320px] mx-auto ${className} `}>{children}</div>
+        <div className={` max-w-330 mx-auto ${className} `}>{children}</div>
     )
 }
 
